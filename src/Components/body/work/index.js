5218cbd8808632c2e1234f77ecdbd33ef54db866
Ranjeet <ranjeetb623@gmail.com>
2021-10-22 10:23:27 +0530

    added work experience

--- src/Components/body/work/index.js
+++ src/Components/body/work/index.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import './work.css'
 import Seperator from '../common/seperator/index'
+import { WorkData } from '../../data/work'
+import WorkCard from './work-card';
 
 function Work() {
+    const data = WorkData;
     return (
         <div className="work">
             <Seperator />
             <label className="section-title" id="work-title">Work</label>
             <div className="work-list">
-                <p>I am <span className="work-imp">actively looking</span> for front-end role internships. I am a team person and love to collaborate. <br/><br/> Currently I have no experience but it will be an honour for me to <span className="work-imp">mention your company</span> in this section :) </p>
-                <img src={require("../../../assets/work.png").default} alt='work svg' className="work-photo" />
+                {data.map(item => {
+                    return (
+                        <WorkCard item={item}/>
+                    )
+                })}
             </div>
         </div>
     )
--- src/Components/data/work.js
+++ src/Components/data/work.js
@@ -1,10 +1,18 @@
 export const WorkData = [
     {
-        company:"",
-        designation:"",
-        dateJoining:"",
-        endJoining:"",
-        companyLogo:"",
-        work:"",
+        company:"The Machine Learning Company",
+        designation:"Web Developement Intern",
+        dateJoining:"Oct 2021",
+        dateEnding:"Present",
+        companyLogo:require("../../assets/Jobs/tmlc.png").default,
+        work:"Working on enhancing the scalability of the website and making it responsive.",
+    },
+    {
+        company:"Your Company Name",
+        designation:"Web Developer",
+        dateJoining:"Today",
+        dateEnding:"",
+        companyLogo:require("../../assets/work.png").default,
+        work:"Currently looking for ReactJS centered role. Will be glad to work with you",
     },
 ]
